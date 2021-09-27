fc56be9153c1790f5faa3a7cf3995fe2940672f1
saifulislammunna <saifulmunna28@gmail.com>
2021-09-27 13:50:54 +0600

    js-string-array-added

---
+++ case-convert.js
@@ -0,0 +1,11 @@
+const anthem = 'Amar Sonar Bangla Ami Tomai Valobashi';
+const search = 'valo';
+
+
+const userInput = 'blACkPink';
+const savedUserName = 'BlackPink';
+// console.log(userInput.toLowerCase());
+// console.log(savedUserName.toLowerCase());
+if (userInput.toLowerCase() == savedUserName.toLowerCase()) {
+    console.log('user exists');
+}
